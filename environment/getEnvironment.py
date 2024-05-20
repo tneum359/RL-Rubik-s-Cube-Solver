46a7f3d664e4cd612fe61573de3516b0cb2b452f
tneum359 <97999741+tneum359@users.noreply.github.com>
2024-05-20 11:43:42 -0400

    Create getEnvironment.py

---
+++ environment/getEnvironment.py
@@ -0,0 +1,12 @@
+
+
+def getEnvironment(environment):
+    if environment == "puzzleN":
+        from environment.PuzzleN import PuzzleN
+        return PuzzleN
+    elif environment == "cubeN":
+        from environment.cubeN import CubeN
+        return CubeN
+    else:
+        ValueError("Invalid Puzzle")
+    
